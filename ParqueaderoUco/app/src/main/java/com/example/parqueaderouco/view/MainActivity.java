c005d83e1e6b388f8484cdebaf5e6e0be181edd7
Dacamo <castrom1998@gmail.com>
2020-03-25 23:38:25 -0500

    Improve Activities

--- ParqueaderoUco/app/src/main/java/com/example/parqueaderouco/view/MainActivity.java
+++ ParqueaderoUco/app/src/main/java/com/example/parqueaderouco/view/MainActivity.java
@@ -2,15 +2,36 @@ package com.example.parqueaderouco.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.parqueaderouco.R;
+import com.example.parqueaderouco.utilities.ActionBarUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBarUtil actionBarUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+    private void initComponents() {
+        actionBarUtil = new ActionBarUtil(this);
+        actionBarUtil.setToolBar(getString(R.string.menu_principal));
+    }
+
+    public void goToTarifaActivity(View view) {
+        Intent intent = new Intent(this,TarifaActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
