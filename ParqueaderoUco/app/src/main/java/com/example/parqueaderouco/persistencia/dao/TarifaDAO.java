3143f9f69fe2ec27971335a2682c666a7897e849
Dacamo <castrom1998@gmail.com>
2020-03-25 23:38:57 -0500

    Improve Dao

--- ParqueaderoUco/app/src/main/java/com/example/parqueaderouco/persistencia/dao/TarifaDAO.java
+++ ParqueaderoUco/app/src/main/java/com/example/parqueaderouco/persistencia/dao/TarifaDAO.java
@@ -1,7 +1,22 @@
 package com.example.parqueaderouco.persistencia.dao;
-
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import com.example.parqueaderouco.entidades.Tarifa;
+import java.util.List;
 
 @Dao
 public interface TarifaDAO {
+    @Insert
+    void insert(Tarifa tarifa);
+
+    @Delete
+    void delete(Tarifa tarifa);
+
+    @Query("DELETE FROM tarifa WHERE idTarifa=:idTarifa")
+    void deleteByIdTarifa(Integer idTarifa);
+
+    @Query("SELECT * FROM tarifa")
+    List<Tarifa> listar();
 }
