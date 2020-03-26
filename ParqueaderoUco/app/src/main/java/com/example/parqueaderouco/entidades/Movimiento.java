24c7714150cae32bba02143c7d197ec5bbcbe1ea
Dacamo <castrom1998@gmail.com>
2020-03-25 20:01:57 -0500

    Added entidades

--- ParqueaderoUco/app/src/main/java/com/example/parqueaderouco/entidades/Movimiento.java
+++ ParqueaderoUco/app/src/main/java/com/example/parqueaderouco/entidades/Movimiento.java
@@ -3,25 +3,21 @@ package com.example.parqueaderouco.entidades;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-
 import com.example.parqueaderouco.persistencia.Tabla;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
-@Entity(tableName = Tabla.TARIFA)
+@Entity(tableName = Tabla.MOVIMIENTO)
 @NoArgsConstructor
-public class Tarifa {
+public class Movimiento {
 
     @PrimaryKey(autoGenerate = true)
     @NonNull
+    @ColumnInfo(name = "idMovimiento")
+    private Integer idMovimiento;
     @ColumnInfo(name = "idTarifa")
     private Integer idTarifa;
-    @ColumnInfo(name = "nombre")
-    private String nombre;
-    @ColumnInfo(name = "precio")
-    private double precio;
 
 }
