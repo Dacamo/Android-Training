0824c5e76587cc471df59a738b408e4c2bdd9647
Dacamo <castrom1998@gmail.com>
2020-04-09 00:14:32 -0500

    Created base project with the views

---
+++ Cetus/app/src/main/java/com/cridacamo/cetus/views/RegisterUserActivity.java
@@ -0,0 +1,16 @@
+package com.cridacamo.cetus.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cridacamo.cetus.R;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
